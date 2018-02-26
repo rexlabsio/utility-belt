85fd1f9e13cab18985c71effa3335754a00e2944
Jodie Dunlop <jodie.dunlop@rexsoftware.com.au>
2018-02-26 10:06:08 +1000

    Optimise for PHP 7

--- src/ObjectUtility.php
+++ src/ObjectUtility.php
@@ -15,32 +15,33 @@ class ObjectUtility
 	 * @param callable $uncastable_caster (optional) A callback to use to cast a value if it otherwise couldn't be
 	 *     cast. If not provided and an unprocessable object is provided an \InvalidArgumentException will be thrown.
 	 *     If the callback does not return an array an InvalidArgumentException will be thrown
-	 * @return array
+     * @throws \InvalidArgumentException
+	 * @return array|mixed
 	 */
 	public static function castToArray($object, callable $uncastable_caster = null)
 	{
-		if (is_array($object)) {
+		if (\is_array($object)) {
 			return $object;
 		}
-		if (is_object($object)) {
+		if (\is_object($object)) {
 			if ($object instanceof \ArrayObject) {
 				return $object->getArrayCopy();
 			}
-			$possible_methods = ["toArray", "getArray", "getAsArray", "getArrayCopy"];
+			$possible_methods = ['toArray', 'getArray', 'getAsArray', 'getArrayCopy'];
 			foreach ($possible_methods as $method_name) {
 				if (method_exists($object, $method_name)) {
-					return (array)call_user_func([$object, $method_name]);
+					return (array)\call_user_func([$object, $method_name]);
 				}
 			}
 
-			if ($uncastable_caster) {
-				$value = call_user_func($uncastable_caster, $object);
-				if (!is_array($value)) {
-					throw new \InvalidArgumentException("Invalid callback. Value returned from castToArray must be an array");
+			if ($uncastable_caster !== null) {
+				$value = $uncastable_caster($object);
+				if (!\is_array($value)) {
+					throw new \InvalidArgumentException('Invalid callback. Value returned from castToArray must be an array');
 				}
 				return $value;
 			}
-			throw new \InvalidArgumentException("Object passed in must either be an array, implement a toArray method which returns an array or extend ArrayObject.");
+			throw new \InvalidArgumentException('Object passed in must either be an array, implement a toArray method which returns an array or extend ArrayObject.');
 		}
 		return (array)$object;
 	}
